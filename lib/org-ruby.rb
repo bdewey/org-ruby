0c07b90745b860da8b3151ce366f051b07275a99
Waldemar Quevedo <waldemar.quevedo@gmail.com>
2012-05-22 21:45:57 +0900

    Bumped gem to 0.6.3 for patch release

--- lib/org-ruby.rb
+++ lib/org-ruby.rb
@@ -3,7 +3,7 @@ unless defined? ::OrgRuby
 module OrgRuby
 
   # :stopdoc:
-  VERSION = '0.6.1'
+  VERSION = '0.6.3'
   LIBPATH = ::File.expand_path(::File.dirname(__FILE__)) + ::File::SEPARATOR
   PATH = ::File.dirname(LIBPATH) + ::File::SEPARATOR
   # :startdoc:
