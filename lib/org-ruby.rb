245f5029a6788a16d4abef3df50a7bdf1e2ef159
Waldemar Quevedo <waldemar.quevedo@gmail.com>
2013-02-17 02:22:06 +0900

    Bump version to 0.8.1 patch release

--- lib/org-ruby.rb
+++ lib/org-ruby.rb
@@ -21,7 +21,7 @@ require 'org-ruby/tilt'
 module OrgRuby
 
   # :stopdoc:
-  VERSION = '0.8.0'
+  VERSION = '0.8.1'
   LIBPATH = ::File.expand_path(::File.dirname(__FILE__)) + ::File::SEPARATOR
   PATH = ::File.dirname(LIBPATH) + ::File::SEPARATOR
   # :startdoc:
