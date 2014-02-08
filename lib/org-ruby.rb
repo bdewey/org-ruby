4f00a89b46973bd65ba8d34ebf4df5ede52b5284
Waldemar Quevedo <waldemar.quevedo@gmail.com>
2014-02-08 17:20:37 +0900

    Bump version to 0.9.0

--- lib/org-ruby.rb
+++ lib/org-ruby.rb
@@ -24,7 +24,7 @@ require 'org-ruby/tilt'
 module OrgRuby
 
   # :stopdoc:
-  VERSION = '0.8.3'
+  VERSION = '0.9.0'
   LIBPATH = ::File.expand_path(::File.dirname(__FILE__)) + ::File::SEPARATOR
   PATH = ::File.dirname(LIBPATH) + ::File::SEPARATOR
   # :startdoc:
